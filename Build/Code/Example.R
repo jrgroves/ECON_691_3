7fd09f59582ec4fb1c53bcb8263ac22c55628ea4
Jeremy R Groves <jgroves@niu.edu>
2025-11-19 12:37:09 -0600

    Finalized Slides

---
+++ Build/Code/Example.R
@@ -0,0 +1,72 @@
+#This script is used to input, clean and prepare our UFO sighting data for use in analysis
+#
+#By: Jeremy Groves
+#Date: November 5, 2025
+
+rm(list = ls())
+
+library(tidyverse)
+library(sf)
+library(tidycensus)
+
+#Getting the Data####
+
+ufo <- read.csv("./Build/Input/scrubbed.csv", header = TRUE, as.is = TRUE)
+bridge <- read.csv("./Build/Input/GEOID Bridge.csv", header = TRUE, as.is = TRUE)
+var <- c("B01002_001", "B01003_001")
+
+for(i in seq(2009,2011,1)) {  
+  acs <- get_acs(geography = "state",
+                 variables = var,
+                 year = i,
+                 geometry = TRUE) %>%
+    mutate(year = i)
+  
+  
+  ifelse(i == 2009,
+         ACS <- acs,
+         ACS <- bind_rows(ACS, acs))
+}
+
+#Cleaning the Data
+
+working <- ufo %>%
+  select(datetime, city, state, country, latitude, longitude) %>%
+  filter(country == "us") %>%
+  mutate(year = str_split_i(str_split_i(datetime, " ", 1), "/", 3)) %>%
+  mutate(year = as.numeric(year)) %>%
+  filter(year > 2008) |>
+  count(year, state) |>
+  rename("sightings" = "n")
+
+census <- ACS %>%
+  select(GEOID, variable, year, estimate, geometry) %>%
+  mutate(variable = case_when(variable == "B01002_001" ~ "median.age",
+                              variable == "B01003_001" ~ "population",
+                              TRUE ~ variable))  %>%
+  pivot_wider(id_cols = c(GEOID, year, geometry), names_from = variable, values_from = estimate) %>%
+  mutate(population = population / 100000)
+
+bridge <- bridge %>%
+  mutate(GEOID = str_pad(as.character(GEOID),2, side = "left", pad = "0" ))
+
+core <- census %>%
+  left_join(., bridge, by = "GEOID") %>%
+  left_join(., working, by = c("state", "year")) %>%
+  mutate(sight_per = sightings / population) %>%
+  relocate(geometry, .after = sight_per) %>%
+  mutate(year = as.character(year),
+         age = case_when(median.age <= 37.2 ~ "1",
+                         TRUE ~ "0")) %>%
+  filter(GEOID != "02" & GEOID != "15" & GEOID != "72") %>%
+  filter(!is.na(sightings))
+  
+save(core, file = "./Build/Output/core.RData")
+
+
+
+
+
+
+
+
